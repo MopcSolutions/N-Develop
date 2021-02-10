b127e1e64e71ff13943686bd17b4f35979deda1e
Mopc <mopc@roboterwerke.su>
2021-02-10 03:53:57 +0200

    api works #3

--- server/ndev-admin/config/routes.rb
+++ server/ndev-admin/config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  
-  # use RESTful routes
-  resources :authors
-
-  resources :notes do
-    member do
-      get :delete
-    end
-  end
-  
-  resources :stories do
-    member do
-      get :delete
-    end
-  end
-  
-  # get 'author/put'
-  get 'authors/get'
+  # get 'authors/index'
+  # get 'authors/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get ':controller(/:action(/:id))'
+
 end
--- server/ndev-admin/test/models/author_note_join_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class AuthorNoteJoinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
